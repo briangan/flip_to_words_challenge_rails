4aad41548ec86f59367469d0d27666e34ea5ad65
Brian Gan <briangangster@gmail.com>
2026-06-01 21:58:19 -0400

    Test: flip_letter_map_test; removed fixtures/flip_letters.yml coz no actual DB table

--- app/models/flip_letter.rb
+++ app/models/flip_letter.rb
@@ -31,11 +31,17 @@ class FlipLetter
   # @return [Boolean]
   def matching_positions?(other_positions)
     other_positions = self.class.parse_positions(other_positions)
-    @positions == other_positions
+    index = 0
+    @positions.all? do|pos|
+      other_value = other_positions[index] || 0
+      index += 1
+      pos == 2 || pos == other_value
+    end
   end
 
 
-  # TODO: Within @positions position value switched to integer value of POSITION_VALUE_MEANINGS
+  # Within @positions position value switched to integer value of POSITION_VALUE_MEANINGS
+  # This ensures @positions has the same length for comparison.
   # @return <Array> with TOTAL_POSITIONS_FOR_LETTER length
   def self.parse_positions(positions)
     if positions.is_a?(Hash) || positions.is_a?(ActionController::Parameters)
---
+++ test/models/flip_letter_map_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class FlipLetterMapTest < ActiveSupport::TestCase
+
+  test "each FlipLetter has valid letters and positions" do
+    flip_letter_map = FlipLetterMap.load_from_file
+    
+    flip_letter_map.map.each do |letter, flip_letter|
+      assert_not_nil letter, "Map key letter '#{letter}' should not be nil"
+      assert_equal letter, flip_letter.letter.upcase, "FlipLetter letter should match map key '#{letter}'"
+      assert_instance_of FlipLetter, flip_letter, "Map value '#{flip_letter}' should be a FlipLetter instance"
+
+      assert_not_nil flip_letter.positions, "Positions for letter '#{letter}' should not be nil"
+      assert_instance_of Array, flip_letter.positions, "Positions for letter '#{letter}' should be an array"
+      assert_not_empty flip_letter.positions, "Positions for letter '#{letter}' should not be empty"
+    end
+  end
+
+  test "map contains expected letters from CSV" do
+    flip_letter_map = FlipLetterMap.load_from_file
+    
+    # Based on the CSV file, these letters should be present
+    expected_letters = %w[B C D E F G H J L O P T U X Y]
+    
+    expected_letters.each do |letter|
+      assert flip_letter_map.map.key?(letter), "Map should contain letter '#{letter}'"
+      assert_not_nil flip_letter_map.map[letter], "Value for letter '#{letter}' should not be nil"
+    end
+
+    b_letter = flip_letter_map.map['B']
+    assert_not_nil b_letter, "Map should contain letter 'B'"
+    exact_positions = b_letter.positions
+    assert_equal exact_positions, b_letter.positions, "Positions for letter 'B' should match expected positions from CSV"
+    assert_equal b_letter.matching_positions?(exact_positions), true, "matching_positions? should return true for exact positions"
+
+    if exact_positions.index(2) # has optional choice
+      flipped_off_positions = exact_positions.map { |pos| pos == 2 ? 0 : pos }
+      assert_equal b_letter.matching_positions?(flipped_off_positions), true, "matching_positions? should return true for flipped off positions"
+      flipped_on_positions = exact_positions.map { |pos| pos == 2 ? 1 : pos }
+      assert_equal b_letter.matching_positions?(flipped_on_positions), true, "matching_positions? should return true for flipped on positions"
+    end
+
+    # Test w/ diff positions
+    flipped_positions = exact_positions.map { |pos| pos == 1 ? 0 : pos == 0 ? 1 : pos }
+    assert_equal b_letter.matching_positions?(flipped_positions), false, "matching_positions? should return false for flipped positions"
+  end
+
+end
