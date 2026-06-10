3b6f0d27b9a2a928d3cace55ffb184befeae80c3
Brian Gan <briangangster@gmail.com>
2026-06-09 23:27:01 -0400

    Flip Letter: form restructured to show demo picture.  Little more integration test.

--- app/helpers/flip_to_words_challenge_helper.rb
+++ app/helpers/flip_to_words_challenge_helper.rb
@@ -1,7 +1,7 @@
 ##
-# Module contains both the controller and helper level methods 
+# Module contains both the controller and helper level methods
 #   to check if the user has passed the Flip to Words Challenge before allowing access to certain pages.
-#   check_flip_to_words_challenge - Alternatively, you can write custom method that uses the logic 
+#   check_flip_to_words_challenge - Alternatively, you can write custom method that uses the logic
 #     in check_flip_to_words_challenge to check the challenge status or react differently.
 module FlipToWordsChallengeHelper
   def flip_to_words_challenge_status
@@ -22,16 +22,16 @@ module FlipToWordsChallengeHelper
     letters.map { |letter| flip_letter_map.map[letter] }
   end
 
-  # before_action method to check if the user has passed the challenge before allowing access to certain pages, which checks 
+  # before_action method to check if the user has passed the challenge before allowing access to certain pages, which checks
   #   session[:flip_to_words_challenge_status] for already passed status.
-  #   If not passed, will be redirected to the home page with an alert message; 
-  #   and would also set sessin[:return_url] to the originally requested URL so that after passing the challenge, 
+  #   If not passed, will be redirected to the home page with an alert message;
+  #   and would also set sessin[:return_url] to the originally requested URL so that after passing the challenge,
   #   user can be redirected back to the originally requested page.
   def check_flip_to_words_challenge
     logger.info "flip_to_words_challenge_status: #{flip_to_words_challenge_status}"
     unless flip_to_words_challenge_passed?
       session[:return_url] = request.fullpath
-      redirect_to root_path
+      redirect_to root_path, alert: t("flip_to_words_challenge.must_pass_message") || "You must pass the Flip to Words Challenge to access this page."
     end
   end
 end
